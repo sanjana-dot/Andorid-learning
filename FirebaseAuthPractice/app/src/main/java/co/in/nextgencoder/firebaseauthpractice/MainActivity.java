bc4cd79abd2cc540a6e007e7fd138c04f54b0450
sanjana-dot <sanjanavaidya44@gmail.com>
2021-08-08 15:34:49 +0530

    Initinal Commit

---
+++ FirebaseAuthPractice/app/src/main/java/co/in/nextgencoder/firebaseauthpractice/MainActivity.java
@@ -0,0 +1,14 @@
+package co.in.nextgencoder.firebaseauthpractice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
